4cde45f93b208095debc92b4d66dcc54c12cd829
David Bonardi <131940263+SamSepi0llll@users.noreply.github.com>
2024-07-10 06:51:01 -0300

    Uploading ex_typescript.ts

---
+++ ex_typescript.ts
@@ -0,0 +1,7 @@
+const multiplicar = (number1: number, number2: number): number => number1 * number2
+
+const saudação = (nome: string): string => `Olá, ${nome}`
+
+console.log(saudação("David"))
+
+console.log(multiplicar(10, 2))
